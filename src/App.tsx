003f6bbc4aba025299f9ae5123bfe864d08c873a
Alex <ol.shevchuk92@gmail.com>
2023-12-01 22:10:20 +0200

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,10 @@ function App() {
     const newTodo = { id: Date.now(), text };
     setTodos([...todos, newTodo]);
   };
+  const test = (text: string) => {
+    const newTodo = { id: Date.now(), text };
+    setTodos([...todos, newTodo]);
+  };
 
   const deleteTodo = (id: number) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
