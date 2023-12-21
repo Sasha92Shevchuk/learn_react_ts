779dfd07f8bf0981143f537670f7507867c6cd99
Alex <ol.shevchuk92@gmail.com>
2023-12-21 22:25:51 +0200

    add decrement

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -6,10 +6,14 @@ export const Counter = () => {
   const increment = () => {
     setCount((prevCount) => prevCount + 1);
   };
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
   return (
     <div>
       <h2>Count: {count}</h2>
       <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Dencrement</button>
     </div>
   );
 };
